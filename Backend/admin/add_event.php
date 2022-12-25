d1f84ef0e579941c47a1012604fa2a02f0fb9011
Devashish Gupta <devashishgupta9822@gmail.com>
2022-12-25 14:52:02 +0530

    feat(admins): add add_event.php

---
+++ Backend/admin/add_event.php
@@ -0,0 +1,51 @@
+<?php
+
+// Establish Connection
+include("db_connection.php");
+
+// Send admin token to access Club Admin
+$admin_token = $_POST['admin_token'];
+
+// Query to validate admin token
+$check = "SELECT * FROM `admins` WHERE `admin_token` = '$admin_token' AND `login_status` = '1' ";
+$result = mysqli_query($conn, $check);
+$count = mysqli_num_rows($result);
+
+// Validate admin token and login status
+if ($count == 1) {
+    $row = mysqli_fetch_assoc($result);
+
+    // Get club id from Admin Data
+    $club_id = $row['admin_id'];
+
+    // Send Event details to be added
+    $event_name = $_POST['event_name'];
+    $event_date_time = $_POST['event_date_time'];
+    $venue = $_POST['venue'];
+    $description = $_POST['description'];
+    $image = $_POST['image'];
+
+    // Query to add event details
+    $add = "INSERT INTO `events`(`event_name`, `event_date_time`, `venue`, `description`, `image`, `club_id`) VALUES ('$event_name','$event_date_time','$venue','$description','$image','$club_id')";
+
+    $add_query = mysqli_query($conn, $add) or die("Error adding the Event.");
+
+    $response = new stdClass();
+    $response->message = "New Event Created";
+    $response->club_id = $club_id;
+    $response->event_name = $event_name;
+    $response->event_date_time = $event_date_time;
+    $response->venue = $venue;
+    $response->description = $description;
+    $response->image = $image;
+    $response_JSON = json_encode($response);
+    echo $response_JSON;
+
+}
+// If invalid token or session
+else {
+    echo "Invalid Token or Session!";
+}
+
+
+?>
