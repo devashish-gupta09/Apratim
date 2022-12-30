adda10fbb8fb0d481eb34f3eb256e305ab9b3813
Devashish Gupta <devashishgupta9822@gmail.com>
2022-12-30 20:07:46 +0530

    feat(admins): additional details/checks in edit_event

--- Backend/admin/edit_event.php
+++ Backend/admin/edit_event.php
@@ -3,6 +3,22 @@
 // Establish Connection
 include("db_connection.php");
 
+// Function to generate integer event_id
+function generateEventIDReverse($final_id)
+{
+    $prefix = 'AP23-E';
+
+    if (substr($final_id, 0, strlen($prefix)) == $prefix) {
+        $final_id = substr($final_id, strlen($prefix));
+    }
+
+    $event_id = (int) $final_id;
+    return $event_id;
+}
+
+// Define response object
+$response = new stdClass();
+
 // Send admin token to access Club Admin
 $admin_token = $_POST['admin_token'];
 
@@ -19,34 +35,53 @@ if ($count == 1) {
     $club_id = $row['admin_id'];
 
     // Send event id to Edit
-    $event_name = $_POST['event_name'];
+    $event_id_char = $_POST['event_id'];
+    // Convert event id to integer-type
+    $event_id = generateEventIDReverse($event_id_char);
 
     // Fetch event details from `events`
-    $query = "SELECT * FROM `events` WHERE `club_id` = '$club_id' AND `event_name` = '$event_name' ";
+    $query = "SELECT * FROM `events` WHERE `club_id` = '$club_id' AND `event_id` = '$event_id' ";
     $result1 = mysqli_query($conn, $query);
     $count1 = mysqli_num_rows($result1);
 
+    // If the event exists
     if ($count1 == 1) {
 
         $details = mysqli_fetch_assoc($result1);
 
         // Initialize with old event values
-        $event_id = $details['event_id'];
+        $event_name = $details['event_name'];
         $event_date_time = $details['event_date_time'];
         $venue = $details['venue'];
         $description = $details['description'];
         $image = $details['image'];
+        $club_id = $details['club_id'];
+        $team_event = $details['team_event'];
+        $team_size = $details['team_size'];
 
-        $response = new stdClass();
-        $response->club_id = $club_id;
-        $response->event_id = $event_id;
 
         // Update Event Name
-        if (isset($_POST['new_event_name'])) {
-            $event_name = $_POST['new_event_name'];
-            $update_event_name = "UPDATE `events` SET `event_name` = '$event_name' WHERE `event_id` = '$event_id' ";
-            $update_event_name1 = mysqli_query($conn, $update_event_name) or die("Error updating the Event Name");
-            $response->event_name = $event_name;
+        if (isset($_POST['event_name'])) {
+            $event_name = $_POST['event_name'];
+
+            // Check if Event Name already exists
+            $check1 = "SELECT * FROM `events` WHERE `event_name` = '$event_name' ";
+            $res1 = mysqli_query($conn, $check1);
+            $count2 = mysqli_num_rows($res1);
+
+            // If no such event exists - UPDATE
+            if ($count2 == 0) {
+                $update_event_name = "UPDATE `events` SET `event_name` = '$event_name' WHERE `event_id` = '$event_id' ";
+                $update_event_name1 = mysqli_query($conn, $update_event_name) or die("Error updating the Event Name");
+            }
+            // Send error and exit in case of duplicate event.
+            else {
+                $response->message = "Can't update event name. Similar event already exists!";
+                $response_JSON = json_encode($response);
+                echo $response_JSON;
+                exit;
+            }
+
         }
 
         // Update Event Date & Time
@@ -54,7 +89,6 @@ if ($count == 1) {
             $event_date_time = $_POST['event_date_time'];
             $update_event_date_time = "UPDATE `events` SET `event_date_time` = '$event_date_time' WHERE `event_id` = '$event_id' ";
             $update_event_date_time1 = mysqli_query($conn, $update_event_date_time) or die("Error updating the Event Date & Time");
-            $response->event_date_time = $event_date_time;
         }
 
         // Update Event Venue
@@ -62,7 +96,6 @@ if ($count == 1) {
             $venue = $_POST['venue'];
             $update_venue = "UPDATE `events` SET `venue` = '$venue' WHERE `event_id` = '$event_id' ";
             $update_venue1 = mysqli_query($conn, $update_venue) or die("Error updating the Event Venue");
-            $response->venue = $venue;
         }
 
         // Update Event Description
@@ -70,7 +103,6 @@ if ($count == 1) {
             $description = $_POST['description'];
             $update_description = "UPDATE `events` SET `description` = '$description' WHERE `event_id` = '$event_id' ";
             $update_description1 = mysqli_query($conn, $update_description) or die("Error updating the Event description");
-            $response->description = $description;
         }
 
         // Update Event Image
@@ -78,22 +110,58 @@ if ($count == 1) {
             $image = $_POST['image'];
             $update_image = "UPDATE `events` SET `image` = '$image' WHERE `event_id` = '$event_id' ";
             $update_image1 = mysqli_query($conn, $update_image) or die("Error updating the Event image");
-            $response->image = $image;
         }
+
+        // Update Team Event or not 
+        if (isset($_POST['team_event'])) {
+            $team_event = $_POST['team_event'];
+            $update_team_event = "UPDATE `events` SET `team_event` = '$team_event' WHERE `event_id` = '$event_id' ";
+            $update_team_event1 = mysqli_query($conn, $update_team_event) or die("Error updating the Event team_event");
+        }
+
+        // Update Team Size
+        if (isset($_POST['team_size'])) {
+            $team_size = $_POST['team_size'];
+            $update_team_size = "UPDATE `events` SET `team_size` = '$team_size' WHERE `event_id` = '$event_id' ";
+            $update_team_size1 = mysqli_query($conn, $update_team_size) or die("Error updating the Event team_size");
+        }
+
+        // Get Event's Club name
+        $query1 = "SELECT `club_name` FROM `clubs` WHERE `club_id` = '$club_id' ";
+        $res2 = mysqli_query($conn, $query1);
+        $row2 = mysqli_fetch_assoc($res2);
+        $club_name = $row2['club_name'];
 
         // Successful updation of the required attributes
         $response->message = "Event details updated successfully";
+        $response->event_id = $event_id;
+        $response->event_name = $event_name;
+        $response->event_date_time = $event_date_time;
+        $response->venue = $venue;
+        $response->description = $description;
+        $response->image = $image;
+        $response->club_name = $club_name;
+        $response->team_event = $team_event;
+        $response->team_size = $team_size;
         $response_JSON = json_encode($response);
         echo $response_JSON;
 
-    } else {
-        echo "No such event found!";
+    }
+    // Send error response and exit if No event with given Event ID is found
+    else {
+        $response->message = "No such event Exists!";
+        $response_JSON = json_encode($response);
+        echo $response_JSON;
+        exit;
     }
 
 }
 // If invalid token or session
 else {
-    echo "Invalid Token or Session!";
+    $response->message = "Invalid Token or Session!";
+    $response_JSON = json_encode($response);
+    echo $response_JSON;
+    exit;
 }
 
 ?>
