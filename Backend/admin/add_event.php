ce0c84b1ba9a7b59eecf74a69ea2faccfc4beb9f
Devashish Gupta <devashishgupta9822@gmail.com>
2022-12-30 16:54:14 +0530

    feat(admins): additional details/checks in add_event

--- Backend/admin/add_event.php
+++ Backend/admin/add_event.php
@@ -3,6 +3,21 @@
 // Establish Connection
 include("db_connection.php");
 
+function generateEventID($event_id)
+{
+    if ($event_id < 10) {
+        $final_id = 'AP23-E00' . $event_id;
+    } elseif ($event_id < 100) {
+        $final_id = 'AP23-E0' . $event_id;
+    } else {
+        $final_id = 'AP23-E' . $event_id;
+    }
+    return $final_id;
+}
+
+// Define response object
+$response = new stdClass();
+
 // Send admin token to access Club Admin
 $admin_token = $_POST['admin_token'];
 
@@ -24,27 +39,72 @@ if ($count == 1) {
     $venue = $_POST['venue'];
     $description = $_POST['description'];
     $image = $_POST['image'];
+    $team_event = $_POST['team_event'];
+    $team_size = $_POST['team_size'];
 
-    // Query to add event details
-    $add = "INSERT INTO `events`(`event_name`, `event_date_time`, `venue`, `description`, `image`, `club_id`) VALUES ('$event_name','$event_date_time','$venue','$description','$image','$club_id')";
+    // Check if Event Name already exists
+    $check1 = "SELECT * FROM `events` WHERE `event_name` = '$event_name' ";
+    $res1 = mysqli_query($conn, $check1);
+    $count1 = mysqli_num_rows($res1);
 
-    $add_query = mysqli_query($conn, $add) or die("Error adding the Event.");
+    if ($count1 == 0) {
+        // Query to add event details
+        $add = "INSERT INTO `events`(`event_name`, `event_date_time`, `venue`, `description`, `image`, `club_id`, `team_event`, `team_size`) VALUES ('$event_name','$event_date_time','$venue','$description','$image','$club_id', '$team_event', '$team_size') ";
+        $add_query = mysqli_query($conn, $add) or die("Error adding the Event.");
 
-    $response = new stdClass();
-    $response->message = "New Event Created";
-    $response->club_id = $club_id;
-    $response->event_name = $event_name;
-    $response->event_date_time = $event_date_time;
-    $response->venue = $venue;
-    $response->description = $description;
-    $response->image = $image;
-    $response_JSON = json_encode($response);
-    echo $response_JSON;
+        // Fetch newly added event's details
+        $fetch_new_event = "SELECT * FROM `events` WHERE `event_name` = '$event_name' AND `club_id` = '$club_id' ";
+        $res = mysqli_query($conn, $fetch_new_event);
+        $row1 = mysqli_fetch_assoc($res);
+
+        //Generate New Event's ID in AP23-EXXX format
+        $event_id = generateEventID($row1['event_id']);
+        // Store other Event details
+        $event_name = $row1['event_name'];
+        $event_date_time = $row1['event_date_time'];
+        $venue = $row1['venue'];
+        $description = $row1['description'];
+        $image = $row1['image'];
+        $club_id = $row1['club_id'];
+        $team_event = $row1['team_event'];
+        $team_size = $row1['team_size'];
+
+        // Get Event's Club name
+        $query = "SELECT `club_name` FROM `clubs` WHERE `club_id` = '$club_id' ";
+        $res2 = mysqli_query($conn, $query);
+        $row2 = mysqli_fetch_assoc($res2);
+        $club_name = $row2['club_name'];
+
+        // Send Event details as response
+        $response->message = "New Event Created";
+        $response->event_id = $event_id;
+        $response->event_name = $event_name;
+        $response->event_date_time = $event_date_time;
+        $response->venue = $venue;
+        $response->description = $description;
+        $response->image = $image;
+        $response->club_name = $club_name;
+        $response->team_event = $team_event;
+        $response->team_size = $team_size;
+        $response_JSON = json_encode($response);
+        echo $response_JSON;
+
+    }
+    // Output error if Similar Event exist in the database
+    else {
+        $response->message = "Event with similar name already exists!!";
+        $response_JSON = json_encode($response);
+        echo $response_JSON;
+        exit;
+    }
 
 }
 // If invalid token or session
 else {
-    echo "Invalid Token or Session!";
+    $response->message = "Invalid Token or Session!";
+    $response_JSON = json_encode($response);
+    echo $response_JSON;
+    exit;
 }
 
 
