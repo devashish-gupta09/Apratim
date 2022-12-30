0377af9ae190796d085fae4f373517328fb910e9
Devashish Gupta <devashishgupta9822@gmail.com>
2022-12-30 14:04:16 +0530

    fix(admins): edit_club scope

--- Backend/admin/edit_club.php
+++ Backend/admin/edit_club.php
@@ -11,7 +11,6 @@ $check = "SELECT * FROM `admins` WHERE `admin_token` = '$admin_token' ";
 $result = mysqli_query($conn, $check);
 $count = mysqli_num_rows($result);
 
-
 // Validate admin token
 if ($count == 1) {
     $row = mysqli_fetch_assoc($result);
@@ -21,7 +20,7 @@ if ($count == 1) {
     $club_name = $row['name'];
 
     // Fetch Club Details from `club`
-    $query = "SELECT * FROM `clubs` WHERE `club_id` = '$club_id' ";
+    $query = "SELECT * FROM `clubs` WHERE `club_id` = '$club_id' AND `club_name` = '$club_name' ";
     $result1 = mysqli_query($conn, $query);
     $details = mysqli_fetch_assoc($result1);
 
@@ -32,24 +31,12 @@ if ($count == 1) {
     $club_image = $details['club_image'];
     $about_club = $details['about_club'];
 
-    $response = new stdClass();
-    $response->club_id = $club_id;
-    $response->club_name = $club_name;
-
-    // Update Head Name
-    if (isset($_POST['head_name'])) {
-        $head_name = $_POST['head_name'];
-        $update_head_name = "UPDATE `clubs` SET `head_name` = '$head_name' WHERE `club_id` = '$club_id' ";
-        $update_head_name1 = mysqli_query($conn, $update_head_name) or die("Error updating the Head Name");
-        $response->head_name = $head_name;
-    }
 
     // Update Contact 
     if (isset($_POST['contact'])) {
         $contact = $_POST['contact'];
         $update_contact = "UPDATE `clubs` SET `contact` = '$contact' WHERE `club_id` = '$club_id' ";
         $update_contact1 = mysqli_query($conn, $update_contact) or die("Error updating the Club Contact");
-        $response->contact = $contact;
     }
 
     // Update Head Image
@@ -57,8 +44,6 @@ if ($count == 1) {
         $head_image = $_POST['head_image'];
         $update_head_image = "UPDATE `clubs` SET `head_image` = '$head_image' WHERE `club_id` = '$club_id' ";
         $update_head_image1 = mysqli_query($conn, $update_head_image) or die("Error updating the Head Image");
-        $response->head_image = $head_image;
-
     }
 
     // Update Club Image
@@ -66,7 +51,6 @@ if ($count == 1) {
         $club_image = $_POST['club_image'];
         $update_club_image = "UPDATE `clubs` SET `club_image` = '$club_image' WHERE `club_id` = '$club_id' ";
         $update_club_image1 = mysqli_query($conn, $update_club_image) or die("Error updating the Club Image");
-        $response->club_image = $club_image;
     }
 
     // Update About Club
@@ -74,11 +58,18 @@ if ($count == 1) {
         $about_club = $_POST['about_club'];
         $update_about_club = "UPDATE `clubs` SET `about_club` = '$about_club' WHERE `club_id` = '$club_id' ";
         $update_about_club1 = mysqli_query($conn, $update_about_club) or die("Error updating the Club Details");
-        $response->about_club = $about_club;
     }
 
-    // Successful details of the required attributes
-    $response->message = "Club details updated successfully";
+    // Send updated club details as response
+    $response = new stdClass();
+    $response->message = "Updated Club details";
+    $response->club_id = $club_id;
+    $response->club_name = $club_name;
+    $response->head_name = $head_name;
+    $response->contact = $contact;
+    $response->head_image = $head_image;
+    $response->club_image = $club_image;
+    $response->about_club = $about_club;
     $response_JSON = json_encode($response);
     echo $response_JSON;
 
