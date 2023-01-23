691337ccac51ee8c5b3d291439d2263137e0ff54
Devashish Gupta <devashishgupta9822@gmail.com>
2023-01-23 22:08:11 +0530

    fix(admin): initialization updated

--- Backend/admin/edit_club.php
+++ Backend/admin/edit_club.php
@@ -1,7 +1,12 @@
 <?php
 
 // Establish Connection
-include("db_connection.php");
+include("../assets/initialize.php");
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+if (!$conn) {
+    die("Connection failed!" . mysqli_connect_error());
+}
 
 function generateClubID($club_id)
 {
--- Backend/admin/edit_event.php
+++ Backend/admin/edit_event.php
@@ -1,7 +1,12 @@
 <?php
 
 // Establish Connection
-include("db_connection.php");
+include("../assets/initialize.php");
+$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+
+if (!$conn) {
+    die("Connection failed!" . mysqli_connect_error());
+}
 
 // Function to generate integer event_id
 function generateEventIDReverse($final_id)
