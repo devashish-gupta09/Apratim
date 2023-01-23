7288a72ca09a66b9a12946635599510a8fdeb08f
Devashish Gupta <devashishgupta9822@gmail.com>
2023-01-23 21:01:17 +0530

    fix(admin): logout checks update

--- Backend/admin/logout.php
+++ Backend/admin/logout.php
@@ -6,35 +6,34 @@ include("db_connection.php");
 // Send admin token to access Club Admin
 $admin_token = $_POST['admin_token'];
 
+// Define response object
+$response = new stdClass();
+
 // Query to validate admin token
-$check = "SELECT * FROM `admins` WHERE `admin_token` = '$admin_token' AND `login_status` = '1' ";
+$check = "SELECT * FROM `admins` WHERE `admin_token` = '$admin_token' ";
 $result = mysqli_query($conn, $check);
 $count = mysqli_num_rows($result);
 
 
 // Validate admin token and login status
 if ($count == 1) {
-    $row = mysqli_fetch_assoc($result);
 
-    // Get admin_id and admin_token from Admin Data
-    $admin_id = $row['admin_id'];
-    $admin_token = $row['admin_token'];
-    $name = $row['name'];
-    $login_status = $row['login_status'];
+    $admin_token = 0;
 
-    // Query to logout
-    $logout = "UPDATE `admins` SET `login_status` = '0' WHERE `admin_token` = '$admin_token' AND `admin_id` = '$admin_id'  ";
-    $result1 = mysqli_query($conn, $logout) or die("Error logging out.");
-
-    $response = new stdClass();
-    $response->message = "$name . ' Admin Logged Out' ";
-    $response_JSON = json_encode($response);
-    echo $response_JSON;
+    if ($admin_token == 0) {
+        $response->message = "Admin Logged out successfully.";
+        $response_JSON = json_encode($response);
+        echo $response_JSON;
+        exit;
+    }
 
 }
 // If invalid token or session
 else {
-    echo "Invalid Token or Session!";
+    $response->message = "Invalid Token or Session!";
+    $response_JSON = json_encode($response);
+    echo $response_JSON;
+    exit;
 }
 
 
